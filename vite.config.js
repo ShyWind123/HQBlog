b78ab7afd66b59393546768feeb08b1a4f4bdb75
ShyWind <2513561399@qq.com>
2024-08-14 14:20:43 +0800

    修改小bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteCompression from 'vite-plugin-compression' c
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
