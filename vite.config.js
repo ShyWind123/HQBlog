f1e4bb68959769ab4a495796799c15ce9f571993
ShyWind <2513561399@qq.com>
2024-08-14 13:54:32 +0800

    启用vite的gzip压缩

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression' c
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      threshold: 1024000 // 对大于 1mb 的文件进行压缩
+    })
+  ],
   resolve: {
     // 配置路径别名
     alias: {
